33dec4076124c27a889bcbb276214456ca06707c
KManishY <my1000088@gmail.com>
2022-09-27 14:56:36 +0530

    updated resume

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -30,10 +30,10 @@ function setActiveStyle(BackgroundColor)
 /* ========================== theme light and dark mode =========================== */
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
-})
+	dayNight.querySelector("i").classList.toggle("fa-moon");
+	dayNight.querySelector("i").classList.toggle("fa-sun");
+	document.body.classList.toggle("dark");
+});
 window.addEventListener("load", () => {
     if(document.body.classList.contains("dark"))
     {
